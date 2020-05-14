97fb261083f7230fbc6fc05ef8450356a194f1cf
janettekaymak <has19003@byui.edu>
2020-05-13 21:46:36 -0400

    commit

--- lesson4/js/today.js
+++ lesson4/js/today.js
@@ -6,4 +6,4 @@ const dayName = days[date.getDay()];
 const monthName = monthNames[date.getMonth()];
 const year = date.getFullYear();
 document.getElementById("today").innerHTML = dayName + ', ' + date.getDate() + ' ' + monthName  + ' ' + year;
-document.getElementById("year").innerHTML = year;
+
