32305778d11e5e8a5c3577701e1bae5ff88460b2
janettekaymak <has19003@byui.edu>
2020-05-19 23:27:37 -0400

    commit

---
+++ lesson5/js/pancakeFriday.js
@@ -0,0 +1,4 @@
+if(dayName == 'Tuesday') {
+    document.getElementById("pancakes").innerHTML = "It's Saturday ya'all! And that means it's time for Preston Pancakes in the Park - 9am at the City Park Pavilion.";
+    document.getElementById("pancakes").classList.add("show");
+ }
