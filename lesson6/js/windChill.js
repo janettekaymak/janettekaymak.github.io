a8be3f4664954f0294ce5a75a38e73e9d1a1be1a
janettekaymak <has19003@byui.edu>
2020-05-25 18:28:37 -0400

    commit

---
+++ lesson6/js/windChill.js
@@ -0,0 +1,13 @@
+(function () {
+    const hightemp = document.getElementById('hightemp').innerHTML;
+    const windspeed = document.getElementById('windspeed').innerHTML;
+    let windchill = 35.74 + (0.6215 * hightemp) - (35.75 * (windspeed ** .16)) + (0.4275 * hightemp * (windspeed ** .16));
+    
+     
+    if (hightemp <= 50 && windspeed > 3) {
+       windchill = Math.round(windchill);
+    } else {
+       windchill = "None";
+    }
+    document.getElementById('windchill').innerHTML = windchill;
+  }());
