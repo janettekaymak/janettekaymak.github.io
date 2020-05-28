37c4a0beac1f7ea225c75a164301fb08dd677de8
janettekaymak <has19003@byui.edu>
2020-05-28 19:43:06 -0400

    commit

---
+++ lesson5a/js/today.js
@@ -0,0 +1,9 @@
+//document.getElementById("updated").innerHTML = "Last Updated " + document.lastModified;
+const date = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+const dayName = days[date.getDay()];
+const monthName = monthNames[date.getMonth()];
+const year = date.getFullYear();
+document.getElementById("today").innerHTML = dayName + ', ' + date.getDate() + ' ' + monthName  + ' ' + year;
+
