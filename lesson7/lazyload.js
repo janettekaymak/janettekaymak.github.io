c0e4f36fc4545b083e19a9c1a56e4d96fd68bbd7
janettekaymak <has19003@byui.edu>
2020-06-01 20:34:53 -0400

    commit

--- lesson7/lazyload.js
+++ lesson7/lazyload.js
@@ -10,8 +10,8 @@ function preloadImage(img) {
 }
 
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 800px 0px"
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
