ff801bcb0234b237ac313fa6af7a952e568debb4
janettekaymak <has19003@byui.edu>
2020-06-02 12:45:11 -0400

    commit

--- lesson7/lazyload.js
+++ lesson7/lazyload.js
@@ -29,3 +29,39 @@ images.forEach(image => {
     imgObserver.observe(image);
 });
 
+
+/*const images = document.querySelectorAll(".gallery img[data-src]");
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (img) => {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = () => {
+      img.removeAttribute('data-src');
+    };
+}
+
+if("IntersectionObserver" in window) {
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entries) => {
+        if (!entries.isIntersecting) {
+            return;
+        } else {
+            loadImages(entries.target);
+            imgObserver.unobserve(entries.target);
+        }
+    });
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}*/
+
