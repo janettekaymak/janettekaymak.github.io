14078ae56f23f1add5f678139c91ebe573b5cf56
janettekaymak <has19003@byui.edu>
2020-06-12 15:15:52 -0400

    commit

--- lesson9/home.js
+++ lesson9/home.js
@@ -1,33 +1,41 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
 
-    //Create elements and content
-    for (let i = 0; i < prophets.length; i++ ) {
-        let card = document.createElement('section');
-        let title = document.createElement('h2');
-        let birthdate = document.createElement('p');
-        let bornAt = document.createElement('p');
-        let photo = document.createElement('img');
+        //Create elements and content
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+                let card = document.createElement('section');
+                let name = document.createElement('h2');
+                let photo = document.createElement('img');
+                let motto = document.createElement('h3');
+                let yearFounded = document.createElement('p');
+                let currentPopulation = document.createElement('p');
+                let averageRainfall = document.createElement('p');
 
-        //Create content
-        birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        bornAt.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-        photo.setAttribute('src', prophets[i].imageurl);
-        photo.setAttribute('alt', prophets[i].name + prophets.lastname + " - " + prophets.order);
-        title.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+                //Create content
+                photo.setAttribute('src', 'Images/' + towns[i].photo);
+                photo.setAttribute('alt', towns[i].name);
+                motto.textContent = '\"' + towns[i].motto + '\"';
+                yearFounded.textContent = 'This wonderful little town was founded in ' + towns[i].yearFounded;
+                currentPopulation.textContent = ', and has a population of only ' + towns[i].currentPopulation;
+                averageRainfall.textContent = 'The average rainfall each year in inches is ' + towns[i].averageRainfall;
 
-        //Put elements in each
-        card.appendChild(title);
-        card.appendChild(birthdate);
-        card.appendChild(bornAt);
-        card.appendChild(photo);
-        document.querySelector('div.cards').appendChild(card);
-    }
-  });
+                //Put elements in each
+                card.appendChild(name);
+                card.appendChild(photo);
+                card.appendChild(motto);
+                card.appendChild(yearFounded);
+                card.appendChild(currentPopulation);
+                card.appendChild(averageRainfall);
+                document.querySelector('div.cards').appendChild(card);
+            }
+        }
+    });
+
+
