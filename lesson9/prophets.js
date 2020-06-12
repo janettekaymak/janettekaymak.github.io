b6f2f7d6f3f7eaf5a881be69178629fd5b61abbb
janettekaymak <has19003@byui.edu>
2020-06-12 08:57:58 -0400

    commit

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -12,21 +12,21 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let birth = document.createElement('p');
-        let birthplace = document.createElement('p');
+        let birthdate = document.createElement('p');
+        let bornAt = document.createElement('p');
         let image = document.createElement('img');
 
         //Create content
-        birth.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        bornAt.textContent = 'Place of Birth: ' + prophets[i].birthplace;
         image.setAttribute('src', prophets[i].imageurl);
         image.setAttribute('alt', prophets[i].name + prophets.lastname + " - " + prophets.order);
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
         //Add elements into section
         card.appendChild(h2);
-        card.appendChild(birth);
-        card.appendChild(birthplace);
+        card.appendChild(birthdate);
+        card.appendChild(bornAt);
         card.appendChild(image);
         document.querySelector('div.cards').appendChild(card);
     }
