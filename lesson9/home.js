d6cb2f2b6246dfe38d4f79a62841777e4079034e
janettekaymak <has19003@byui.edu>
2020-06-14 19:45:36 -0400

    commit

--- lesson9/home.js
+++ lesson9/home.js
@@ -23,9 +23,9 @@ fetch(requestURL)
                 photo.setAttribute('alt', towns[i].name);
                 name.textContent = 'Welcome to ' + towns[i].name;
                 motto.textContent = '\"' + towns[i].motto + '\"';
-                yearFounded.textContent = 'Wonderful little town, founded in ' + towns[i].yearFounded + '.';
-                currentPopulation.textContent = "There's lots of room, population " + towns[i].currentPopulation + '.';
-                averageRainfall.textContent = 'Average yearly rainfall is ' + towns[i].averageRainfall + '".';
+                yearFounded.textContent = 'Wonderful little town, founded in ' + towns[i].yearFounded;
+                currentPopulation.textContent = "There's lots of room, population " + towns[i].currentPopulation;
+                averageRainfall.textContent = 'Average yearly rainfall is ' + towns[i].averageRainfall;
 
                 //Put elements in section
                 card.appendChild(photo);
