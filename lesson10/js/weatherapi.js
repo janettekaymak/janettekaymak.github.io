2e1694adc03f2eee25d9380053d17623141c6995
janettekaymak <has19003@byui.edu>
2020-06-17 16:42:34 -0400

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,10 +1,9 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=Preston,us&APPID=abe726d4fa97f7d629527a984b3b6ae0';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Preston,us&APPID=abe726d4fa97f7d629527a984b3b6ae0';
 fetch(apiURL)
 .then((response) => response.json())
 .then((preston) => {
   console.log(preston);
-});
-
+  
 document.getElementById('current-temp').textContent = preston.main.temp;
 
 const imagesrc = 'https://openweathermap.org/img/w/' + preston.weather[0].icon + '.png';  // note the concatenation
@@ -12,4 +11,8 @@ const desc = preston.weather[0].description;  // note how we reference the weath
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
+
+});
+
+
 
