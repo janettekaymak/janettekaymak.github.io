30035e961181d4e978fccf1b4d6041c5444f5b5d
janettekaymak <has19003@byui.edu>
2020-06-17 17:12:01 -0400

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Preston,us&APPID=abe726d4fa97f7d629527a984b3b6ae0';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Preston,us&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0';
 fetch(apiURL)
 .then((response) => response.json())
 .then((preston) => {
