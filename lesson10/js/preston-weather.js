5716a070b106e9113f1d7c666a1be44c65f78fa1
janettekaymak <has19003@byui.edu>
2020-06-18 12:00:08 -0400

    commit

--- lesson10/js/preston-weather.js
+++ lesson10/js/preston-weather.js
@@ -14,3 +14,23 @@ document.getElementById('conditions').textContent = preston.weather[0].main;
 
 });
 
+
+
+
+
+
+
+/*Windchill*/
+(function windChill () {
+  const hightemp = document.getElementById('hightemp').innerHTML;
+  const windspeed = document.getElementById('windspeed').innerHTML;
+  let windchill = 35.74 + (0.6215 * hightemp) - (35.75 * (windspeed ** .16)) + (0.4275 * hightemp * (windspeed ** .16));
+  
+   
+  if (hightemp <= 80 && windspeed > 0) {
+     windChill = Math.round(windChill) + "&#8457";
+  } else {
+     windChill = "N/A";
+  }
+  document.getElementById('windchill').innerHTML = windchill;
+}());
