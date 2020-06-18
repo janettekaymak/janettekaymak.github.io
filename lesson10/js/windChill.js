a7c7d15308043ce120b7d797cd4f3cd9333ed965
janettekaymak <has19003@byui.edu>
2020-06-17 21:10:08 -0400

    commit

--- lesson10/js/windChill.js
+++ lesson10/js/windChill.js
@@ -4,7 +4,7 @@
    let windchill = 35.74 + (0.6215 * hightemp) - (35.75 * (windspeed ** .16)) + (0.4275 * hightemp * (windspeed ** .16));
    
     
-   if (hightemp <= 50 && windspeed > 3) {
+   if (hightemp <= 80 && windspeed > 3) {
       windchill = Math.round(windchill) + "&#8457";
    } else {
       windchill = "N/A";
