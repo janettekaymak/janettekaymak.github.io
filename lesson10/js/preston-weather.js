9f024cea7ce69ea62f4c77d47e1c89380c30a5ff
janettekaymak <has19003@byui.edu>
2020-06-19 12:22:24 -0400

    commit

--- lesson10/js/preston-weather.js
+++ lesson10/js/preston-weather.js
@@ -18,6 +18,32 @@ document.getElementById('conditions').textContent = preston.weather[0].main;
 
 
 
+/*const CURRENT_LOCATION = document.getElementsByClassName('weather-content__overview')[0];
+const CURRENT_TEMP = document.getElementsByClassName('weather-content__temp')[0];
+const FORECAST = document.getElementsByClassName('component__forecast-box')[0];
+const URL = "https://api.openweathermap.org/data/2.5/forecast/daily?" +
+"q=Preston,us&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0";
+
+
+  // render each daily forecast
+  forecast.forEach(day => {
+    let date = new Date(day.dt * 1000);
+    let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+    let name = days[date.getDay()];
+    let dayBlock = document.createElement("div");
+    dayBlock.className = 'forecast__item';
+    dayBlock.innerHTML =
+      `<div class="forecast-item__heading">${name}</div>
+      <div class="forecast-item__info">
+      <i class="wi ${applyIcon(day.weather[0].icon)}"></i>
+      <span class="degrees">${Math.round(day.temp.day)}
+      <i class="wi wi-degrees"></i></span></div>`;
+    FORECAST.appendChild(dayBlock);
+  });
+}
+*/
+
+
 
 
 /*Windchill*/
