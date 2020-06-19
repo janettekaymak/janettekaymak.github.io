735d5a92806403be70a4d9537d3554351e34d5cb
janettekaymak <has19003@byui.edu>
2020-06-19 13:46:26 -0400

    commit

--- lesson10/js/preston-weather.js
+++ lesson10/js/preston-weather.js
@@ -16,47 +16,7 @@ document.getElementById('conditions').textContent = preston.weather[0].main;
 
 
 
-/*function GetDays(startDate, daysToAdd) {
-  var weekDays = [];
-
-  for (var i = 0; i <= daysToAdd; i++) {
-      var currentDate = new Date();
-      currentDate.setDate(startDate.getDate() + i);
-      aryDates.push(DayAsString(currentDate.getDay()) + ", " + currentDate.getDate() + " " + MonthAsString(currentDate.getMonth()) + " " + currentDate.getFullYear());
-  }
-
-  return weekDays;
-}
-
-function DayAsString(dayIndex) {
-  var weekdays = new Array(7);
-  weekdays[0] = "Sunday";
-  weekdays[1] = "Monday";
-  weekdays[2] = "Tuesday";
-  weekdays[3] = "Wednesday";
-  weekdays[4] = "Thursday";
-  weekdays[5] = "Friday";
-  weekdays[6] = "Saturday";
-
-  return weekdays[dayIndex];
-}
-
-var startDate = new Date();
-var aryDates = GetDates(startDate, 7);
-console.log(aryDates);​
-​
-
-/*function days(current) {
-  var week = new Array();
-  // Starting Monday not Sunday 
-  var first = ((current.getDate() - current.getDay()) + 1);
-  for (var i = 0; i < 7; i++) {
-    week.push(
-      new Date(current.setDate(first++))
-    );
-  }
-  return week;
-}*/
+/*Functions to get days of the week and show on 5dayforecast table*/
 function getWeekDay1(date){
   //Create an array containing each day, starting with Sunday.
   var weekdays = new Array(
@@ -112,34 +72,6 @@ document.getElementById('day2').innerHTML = getWeekDay2(date);
 document.getElementById('day3').innerHTML = getWeekDay3(date);
 document.getElementById('day4').innerHTML = getWeekDay4(date);
 document.getElementById('day5').innerHTML = getWeekDay5(date);
-
-
-
-
-/*const CURRENT_LOCATION = document.getElementsByClassName('weather-content__overview')[0];
-const CURRENT_TEMP = document.getElementsByClassName('weather-content__temp')[0];
-const FORECAST = document.getElementsByClassName('component__forecast-box')[0];
-const URL = "https://api.openweathermap.org/data/2.5/forecast/daily?" +
-"q=Preston,us&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0";
-
-
-  // render each daily forecast
-  forecast.forEach(day => {
-    let date = new Date(day.dt * 1000);
-    let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-    let name = days[date.getDay()];
-    let dayBlock = document.createElement("div");
-    dayBlock.className = 'forecast__item';
-    dayBlock.innerHTML =
-      `<div class="forecast-item__heading">${name}</div>
-      <div class="forecast-item__info">
-      <i class="wi ${applyIcon(day.weather[0].icon)}"></i>
-      <span class="degrees">${Math.round(day.temp.day)}
-      <i class="wi wi-degrees"></i></span></div>`;
-    FORECAST.appendChild(dayBlock);
-  });
-}
-*/
 
 
 
