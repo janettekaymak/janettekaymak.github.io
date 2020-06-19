667e2ea1ef0c3e7e68557557d26bf5535325ff12
janettekaymak <has19003@byui.edu>
2020-06-19 13:42:45 -0400

    commit

--- lesson10/js/preston-weather.js
+++ lesson10/js/preston-weather.js
@@ -16,6 +16,104 @@ document.getElementById('conditions').textContent = preston.weather[0].main;
 
 
 
+/*function GetDays(startDate, daysToAdd) {
+  var weekDays = [];
+
+  for (var i = 0; i <= daysToAdd; i++) {
+      var currentDate = new Date();
+      currentDate.setDate(startDate.getDate() + i);
+      aryDates.push(DayAsString(currentDate.getDay()) + ", " + currentDate.getDate() + " " + MonthAsString(currentDate.getMonth()) + " " + currentDate.getFullYear());
+  }
+
+  return weekDays;
+}
+
+function DayAsString(dayIndex) {
+  var weekdays = new Array(7);
+  weekdays[0] = "Sunday";
+  weekdays[1] = "Monday";
+  weekdays[2] = "Tuesday";
+  weekdays[3] = "Wednesday";
+  weekdays[4] = "Thursday";
+  weekdays[5] = "Friday";
+  weekdays[6] = "Saturday";
+
+  return weekdays[dayIndex];
+}
+
+var startDate = new Date();
+var aryDates = GetDates(startDate, 7);
+console.log(aryDates);​
+​
+
+/*function days(current) {
+  var week = new Array();
+  // Starting Monday not Sunday 
+  var first = ((current.getDate() - current.getDay()) + 1);
+  for (var i = 0; i < 7; i++) {
+    week.push(
+      new Date(current.setDate(first++))
+    );
+  }
+  return week;
+}*/
+function getWeekDay1(date){
+  //Create an array containing each day, starting with Sunday.
+  var weekdays = new Array(
+      "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  );
+  //Use the getDay() method to get the day.
+  var day = date.getDay();
+  //Return the element that corresponds to that index.
+  return weekdays[day];
+}
+function getWeekDay2(date){
+  //Create an array containing each day, starting with Monday.
+  var weekdays = new Array(
+      "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+  );
+  //Use the getDay() method to get the day.
+  var day = (date).getDay();
+  //Return the element that corresponds to that index.
+  return weekdays[day];
+}
+function getWeekDay3(date){
+  //Create an array containing each day, starting with Tuesday.
+  var weekdays = new Array(
+     "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",  "Monday" 
+  );
+  //Use the getDay() method to get the day.
+  var day = (date).getDay();
+  //Return the element that corresponds to that index.
+  return weekdays[day];
+}
+function getWeekDay4(date){
+  //Create an array containing each day, starting with Wednesday.
+  var weekdays = new Array(
+     "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",  "Monday", "Tuesday" 
+  );
+  //Use the getDay() method to get the day.
+  var day = (date).getDay();
+  //Return the element that corresponds to that index.
+  return weekdays[day];
+}
+function getWeekDay5(date){
+  //Create an array containing each day, starting with thursday.
+  var weekdays = new Array(
+     "Thursday", "Friday", "Saturday", "Sunday",  "Monday", "Tuesday", "Wednesday"
+  );
+  //Use the getDay() method to get the day.
+  var day = (date).getDay();
+  //Return the element that corresponds to that index.
+  return weekdays[day];
+}
+document.getElementById('day1').innerHTML = getWeekDay1(date);
+document.getElementById('day2').innerHTML = getWeekDay2(date);
+document.getElementById('day3').innerHTML = getWeekDay3(date);
+document.getElementById('day4').innerHTML = getWeekDay4(date);
+document.getElementById('day5').innerHTML = getWeekDay5(date);
+
+
 
 
 /*const CURRENT_LOCATION = document.getElementsByClassName('weather-content__overview')[0];
