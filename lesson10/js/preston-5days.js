981af3a2b0d0b24fe5f773147e32a32649b9d3e0
janettekaymak <has19003@byui.edu>
2020-06-20 16:43:30 -0400

    commit

--- lesson10/js/preston-5days.js
+++ lesson10/js/preston-5days.js
@@ -1,7 +1,1 @@
-/*const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=abe726d4fa97f7d629527a984b3b6ae0';
-fetch(apiURL)
-.then((response) => response.json())
-.then((data) => {
-  console.log(data);
- 
-  document.getElementById('high1').textContent = "this is not working"*/
+
