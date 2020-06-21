795c8f242e2f38840153875dd454ab48f88dc5e3
janettekaymak <has19003@byui.edu>
2020-06-20 20:05:54 -0400

    commit

--- lesson10/js/preston-weather.js
+++ lesson10/js/preston-weather.js
@@ -1,3 +1,4 @@
+
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Preston,us&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0';
 fetch(apiURL)
 .then((response) => response.json())
@@ -91,6 +92,8 @@ document.getElementById('day5').innerHTML = getWeekDay5(date);
   }
   document.getElementById('windchill').innerHTML = wc;
 }());
+
+
 
 
 
