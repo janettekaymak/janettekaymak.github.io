5da78cfd30db49dae4cadc7cdd7b388984e0f1ce
janettekaymak <has19003@byui.edu>
2020-06-21 19:45:25 -0400

    initial commit

---
+++ lesson11/js/soda-springs-weather.js
@@ -0,0 +1,33 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0';
+fetch(apiURL)
+.then((response) => response.json())
+.then((sodasprings) => {
+  console.log(sodasprings);
+  
+document.getElementById('current-temp').textContent = Math.round(sodasprings.main.temp);
+document.getElementById('hightemp').textContent = Math.round(sodasprings.main.temp_max);
+document.getElementById('humidity').textContent = fishhaven.main.humidity;
+document.getElementById('windspeed').textContent = Math.round(sodasprings.wind.speed);
+document.getElementById('conditions').textContent = sodasprings.weather[0].main;
+document.getElementById('feelsLike').textContent = Math.round(sodasprings.main.feels_like);
+
+windChill();
+});
+ 
+
+/*windchill------------------------------------*/
+function windChill (){
+  const t = document.getElementById('current-temp').innerHTML;
+  const ws = document.getElementById('windspeed').innerHTML;
+  var wc = 35.74 + (0.6215 * t) - (35.75 * (ws ** .16)) + (0.4275 * t * (ws ** .16));
+        
+  if (t <= 50 && ws > 3) {
+     var wc = Math.round(wc) + '°F';
+  } 
+    else {
+     wc = "N/A";
+  }
+  document.getElementById('windchill').textContent = wc;
+};
+
+
