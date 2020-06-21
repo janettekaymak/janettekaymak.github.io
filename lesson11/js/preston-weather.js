dc8ac83600f47576a87b15e33f2356cab532d3e1
janettekaymak <has19003@byui.edu>
2020-06-20 23:18:21 -0400

    initial commit

---
+++ lesson11/js/preston-weather.js
@@ -0,0 +1,53 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Preston,us&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0';
+fetch(apiURL)
+.then((response) => response.json())
+.then((preston) => {
+  console.log(preston);
+  
+document.getElementById('current-temp').textContent = Math.round(preston.main.temp);
+document.getElementById('hightemp').textContent = Math.round(preston.main.temp_max);
+document.getElementById('humidity').textContent = preston.main.humidity;
+document.getElementById('windspeed').textContent = Math.round(preston.wind.speed);
+document.getElementById('conditions').textContent = preston.weather[0].main;
+document.getElementById('feelsLike').textContent = Math.round(preston.main.feels_like);
+
+windChill();
+});
+ 
+
+/*windchill------------------------------------*/
+function windChill (){
+  const t = document.getElementById('current-temp').innerHTML;
+  const ws = document.getElementById('windspeed').innerHTML;
+  var wc = 35.74 + (0.6215 * t) - (35.75 * (ws ** .16)) + (0.4275 * t * (ws ** .16));
+        
+  if (t <= 50 && ws > 3) {
+     var wc = Math.round(wc) + '°F';
+  } 
+    else {
+     wc = "N/A";
+  }
+  document.getElementById('windchill').textContent = wc;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson11/js/weatherapi.js
@@ -0,0 +1,24 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Preston,us&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0';
+fetch(apiURL)
+.then((response) => response.json())
+.then((preston) => {
+  console.log(preston);
+  
+document.getElementById('current-temp').textContent = preston.main.temp;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + preston.weather[0].icon + '.png';  // note the concatenation
+const desc = preston.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+
+});
+
+
+
+
+
+
+
+
+   
