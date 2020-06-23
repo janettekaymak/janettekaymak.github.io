f6a925568429e5ba1fc1c32f72da5004764e76b6
janettekaymak <has19003@byui.edu>
2020-06-23 14:49:47 -0400

    commit

--- setup/app.js
+++ setup/app.js
@@ -2,6 +2,9 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
+//get random number between 0 - 3
 btn.addEventListener('click', function(){
-    console.log(document.body);
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 })
