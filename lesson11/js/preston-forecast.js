aa70509c0cf6d0d43033059bcfc8181738619755
janettekaymak <has19003@byui.edu>
2020-06-24 15:08:01 -0400

    commit

--- lesson11/js/preston-forecast.js
+++ lesson11/js/preston-forecast.js
@@ -61,7 +61,7 @@ document.getElementById('day5').innerHTML = getWeekDay5(date);
 
 
 //Fetch JSON weather data from openweathermap
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?q=Preston,us&cnt=6&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0';
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?q=Preston,us&cnt=5&units=imperial&APPID=abe726d4fa97f7d629527a984b3b6ae0';
 
 fetch(apiURL)
 .then((response) => response.json())
