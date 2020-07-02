5f2738e0640d476051d0eb9037651b5068bf4e5f
janettekaymak <has19003@byui.edu>
2020-07-02 10:24:36 -0400

    commit

--- lesson13/js/toggleMenu.js
+++
@@ -1,8 +0,0 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation');
-const menu = document.getElementById('menU');
-
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('responsive');
-    menu.classList.toggle("change");
-}, false);
