4e0ece63e304faf2b30bea5b304e74915eab0edd
Kaymak <janettekaymak@live.com>
2021-02-04 12:31:57 -0500

    commit

--- zen-garden/js/webfontload.js
+++ zen-garden/js/webfontload.js
@@ -1,7 +1,7 @@
 WebFont.load({
     google: {
       families: [
-         "Raleway", "Staatliches", "Limelight"
+         "Raleway", "Staatliches", "Limelight", "Oleo Script"
       ]
     }
   });
